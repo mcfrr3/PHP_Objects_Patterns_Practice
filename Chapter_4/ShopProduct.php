f0524487fe1a9c7d3d69420d6252e62ac7f07380
Mike <m.ferrete88@gmail.com>
2017-09-14 11:50:28 +0200

    add: practice from chapter 4

---
+++ Chapter_4/ShopProduct.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 9/14/17
+ * Time: 11:25 AM
+ */
+
+class ShopProduct
+{
+    private $id = 0;
+
+    public function setID(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public static function getInstance(int $id, \PDO $pdo): ShopProduct
+    {
+        $stmt = $pdo->prepare("select * from products where id=?");
+        $result = $stmt->execute([$id]);
+        $row = $stmt->fetch();
+        if (empty($row)) {
+            return null;
+        }
+
+        if ($row['type'] == "book") {
+            $product = new BookProduct(
+                $row['title'],
+                $row['firstname'],
+                $row['mainname'],
+                (float) $row['price'],
+                (int) $row['numpages']
+            );
+        } elseif ($row['type'] == "cd") {
+            $product = new CdProduct(
+                $row['title'],
+                $row['firstname'],
+                $row['mainname'],
+                (float) $row['price'],
+                (int) $row['playlength']
+            );
+        } else {
+            $firstname = (is_null($row['firstname'])) ? "" : $row['firstname'];
+            $product = new ShopProduct(
+                $row['title'],
+                $firstname,
+                $row['mainname'],
+                (float) $row['price']
+            );
+        }
+
+        $product->setID((int) $row['id']);
+        $product->setDiscount((int) $row['discount']);
+
+        return $product;
+    }
+}
